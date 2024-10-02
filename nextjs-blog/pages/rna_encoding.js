8f4dfd6be416527b73d64674edcf5fda3d7e71a7
aygalic <aygalic@hotmail.fr>
2024-10-02 12:30:12 +0200

    feat: adding dynamic page about RNA encoding

--- nextjs-blog/pages/rna_encoding.js
+++ nextjs-blog/pages/rna_encoding.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+//import '../../nextjs-blog/styles/global.css';
 
 export default function Home() {
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
@@ -8,41 +9,21 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Aygalic Jara</title>
+        <title>Encoding RNA data</title>
         <link rel="icon" href={`${basePath}/favicon.ico`} />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Aygalic Jara
+          Encoding RNA data.
         </h1>
-        <h2>
-          My best projects!
-        </h2>
 
         <p className={styles.description}>
-          Find out about my latest work!
+          The evolution of the latent representation of BRCA data throughout the training procedure!
         </p>
 
-        <div className={styles.grid}>
+        <iframe src="https://aygalic.github.io/biosequence_encoding/pca_animation.html" title="description" className={styles.subframe}></iframe>
 
-          <a href="https://acrabstracts.org/abstract/identification-of-molecular-biomarkers-for-sjogrens-disease-stratification-via-a-deep-learning-foundation-model-dedicated-to-immune-mediated-and-inflammatory-disease/" className={styles.card}>
-            <h3>My first peer reviewed paper! &rarr;</h3>
-            <p>Identification of Molecular Biomarkers for Sjögren’s Disease Stratification via a Deep Learning Foundation Model Dedicated to Immune-Mediated and Inflammatory Disease.</p>
-          </a>
-
-          <a href="https://hdl.handle.net/10589/214686" className={styles.card}>
-            <h3>My Thesis &rarr;</h3>
-            <p>Genetic algorithm-driven auto-encoders: unraveling complex patterns in Parkinson's and breast cancer data</p>
-          </a>
-
-          <a href={`${basePath}/rna_encoding`} className={styles.card}>
-            <h3>Some cool ML approach on Transcriptomic data &rarr;</h3>
-            <p>Evolution of latent representation of BRCA data through encoder over the training procedure.</p>
-          </a>
-
-
-        </div>
       </main>
 
       <footer>
