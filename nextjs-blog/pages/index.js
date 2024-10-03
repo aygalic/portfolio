e253ade92b9332514b0240e4ba784a04a9e1bee1
aygalic <aygalic@hotmail.fr>
2024-10-03 11:32:10 +0200

    feat: switch to global css

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -67,29 +67,6 @@ export default function Home() {
       </main>
 
       <Footer />
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
